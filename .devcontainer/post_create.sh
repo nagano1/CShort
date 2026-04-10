1c50cf5c7c131585e48e8c095a0fc62eda225e81
Your Name <you@example.com>
2026-04-10 09:35:10 +0900

    post_create.sh

---
+++ .devcontainer/post_create.sh
@@ -0,0 +1,10 @@
+echo $0
+containerWorkspaceFolder=$1
+build_persistent=$2
+echo $containerWorkspaceFolder
+echo $build_persistent
+     
+sudo rm -rf /tmp/build
+mkdir -p $containerWorkspaceFolder/$build_persistent
+sudo chown vscode:vscode $containerWorkspaceFolder/$build_persistent
+ln -s $containerWorkspaceFolder/$build_persistent /tmp/build
