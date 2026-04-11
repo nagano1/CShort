1b664879081800502db3c77d737665dd8182ce26
Your Name <you@example.com>
2026-04-11 17:25:54 +0900

    ok

--- .devcontainer/build_in_devcontainer.sh
+++ .devcontainer/build_in_devcontainer.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
 # This project uses '/tmp/build' for build artifacts by default, which is a temporary directory that is cleared when the container is removed.
+echo "-------------------------------------------------------"
 BUILD_DIR="/tmp/build/"
-echo "-------------------------------------------------------"
-echo "Project: CShort"
 echo "Build Directory: $BUILD_DIR"
-echo "-------------------------------------------------------"
+
 # Configure the project
 echo "[1/2] Configuring CMake..."
 cmake -B "$BUILD_DIR" -DCMAKE_EXPORT_COMPILE_COMMANDS=ON
