bf5f7169c0362a597acaf92daf6d8479d5ed1c58
Your Name <you@example.com>
2026-05-27 16:41:05 +0900

    comment

--- .devcontainer/post_create.sh
+++ .devcontainer/post_create.sh
@@ -4,6 +4,9 @@ echo $containerWorkspaceFolder
 echo $build_persistent
      
 sudo rm -rf /tmp/build
+# Create the persistent build directory and set permissions.
 mkdir -p $containerWorkspaceFolder/$build_persistent
 sudo chown vscode:vscode $containerWorkspaceFolder/$build_persistent
+# Create a symbolic link to the persistent build directory. 
+# This allows build tools to use /tmp/build as the build directory, while the actual files are stored in the persistent location. 
 ln -s $containerWorkspaceFolder/$build_persistent /tmp/build
