1bb5c72eb95f56fc5c932631c635d26043ff7730
nagano1 <nagano1@users.noreply.github.com>
2026-07-15 05:44:45 +0900

    rename

--- src/script_engine/StackMemory.cpp
+++ src/script_engine/StackMemory.cpp
@@ -146,16 +146,16 @@ namespace cshort {
     // this is used for reading values from the stack, such as local variables or function arguments without popping them.
     bool StackMemory::moveFromStack(int offsetFromBase, int byteCount, st_byte* ptr) const
     {
-        auto* target = this->stackBasePointer + offsetFromBase;
+        auto* dataPosition = this->stackBasePointer + offsetFromBase;
         auto* stackEnd = this->stackChunk + this->stackSize;
-        // ensure that the stackChunk is not null, the byteCount is positive, and the target address is within the valid stack range.
+        // ensure that the stackChunk is not null, the byteCount is positive, and the dataPosition address is within the valid stack range.
         assert(this->stackChunk != nullptr);
         assert(byteCount > 0);
         // https://tamaron.hatenablog.com/entry/2020/04/02/003805
         // https://zenn.dev/yasu01/articles/1f92df34f54c31
-        assert(target >= this->stackPointer);
-        assert(target >= this->stackChunk);
-        //assert(target + byteCount <= stackEnd); // this assertion is commented out because it may not hold true for negative offsets, which are used to access local variables and function arguments on the stack.
+        assert(dataPosition >= this->stackPointer);
+        assert(dataPosition >= this->stackChunk);
+        assert(dataPosition + byteCount <= stackEnd);
 
         if (byteCount == 1) { // 8bit
             *(uint8_t*)(ptr) = *(uint8_t*)(this->stackBasePointer + offsetFromBase);
