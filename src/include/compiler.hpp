82e5410e62faebdfebb52b5ffa280721cc3d3261
nagano1 <nagano1@users.noreply.github.com>
2026-07-18 06:02:24 +0900

    Prepare for compiler (#39)
    * ok
    
    * refactor

---
+++ src/include/compiler.hpp
@@ -0,0 +1,25 @@
+﻿#pragma once
+
+#include <stdlib.h>
+#include "ParseUtil.hpp"
+#include "common.hpp"
+#include "parser.hpp"
+#include "types.hpp"
+
+namespace cshort
+{
+    /*
+    struct ScriptRunner {
+        static int64_t runScriptWithLength(const char* script, int byteLength);
+
+        template<std::size_t SIZE>
+        static int64_t runScript(const char(&text)[SIZE])
+        {
+            return runScriptWithLength(text, SIZE - 1);
+        }
+    };
+    */
+    struct CompilerForLLVM {
+        static char *compile(DocumentStruct *document, ParseContext *context);
+    };
+}
---
+++ src/tests/compiler_test.cpp
@@ -0,0 +1,60 @@
+#include <cstdio>
+
+#include "parser.hpp"
+#include "ParseUtil.hpp"
+#include "compiler.hpp"
+
+using namespace cshort;
+
+void callTests();
+
+int main()
+{
+    printf("-------------------- Compiler tests ---------------------\n");
+    fflush(stdout);
+
+    callTests();
+
+    printf("-------------------- passed successfully ---------------------\n");
+    return 0;
+}
+
+
+
+void testCompileLLVM1() {
+            constexpr char source[] = R"(
+fn Main()
+{
+    i64 a = 100 // implicit conversion to i64
+    return a
+})";
+
+    auto *document = Alloc::newDocument(DocumentType::CodeDocument);
+    auto *context = document->context;
+
+    DocumentUtils::parseText(document, source, (int)strlen(source));
+
+    Validator::validateScript(document);
+
+
+    char *outputText = CompilerForLLVM::compile(document, context);
+    // currently, the outputText is not used, but in the future, validation and execution will be performed
+    printf("outputText = %s\n", outputText);
+
+    Alloc::deleteDocument(document);
+}
+
+void checkSemanticError(const char* str) {
+
+}
+
+#define CheckTextEq(x) checkTextEquality(#x, x)
+void callTests()
+{
+    testCompileLLVM1();
+
+    /*
+    checkSemanticError(R"(fn Main() { int a = 5
+        int a = 6})");
+    */
+}
